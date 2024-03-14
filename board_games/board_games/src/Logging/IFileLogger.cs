dc640eaaf17277858c28113c695e3e807c16f12e
914LauranDavid <david.lauran@stud.ubbcluj.ro>
2024-03-15 00:38:37 +0200

    simple logging with SimpleFileLogger

---
+++ board_games/board_games/src/Logging/IFileLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace board_games.src.Log
+{
+    /// <summary>
+    /// Interface for a simple logger. Method "shutDown" should always be called when the logger will no longer be used.
+    /// Otherwise, log messages might be lost
+    /// </summary>
+    public interface IFileLogger
+    {
+        void Log(string message, LogLevel logLevel);
+        void Shutdown();
+        string GetFileName();
+        void ChangeFileName(string newFileName);
+    }
+}
---
+++ board_games/board_games/src/Logging/LogLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace board_games.src.Log
+{
+    public enum LogLevel
+    {
+        Info,
+        Warn,
+        Error
+    }
+}
