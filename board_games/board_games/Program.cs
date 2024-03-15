8f8a411da7c97627433434ee6084296dab5dc11e
lazarbianca <161320992+lazarbianca@users.noreply.github.com>
2024-03-15 12:20:46 +0200

    Implemented Feature Toggle Module

--- board_games/board_games/Program.cs
+++ board_games/board_games/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace board_games
+﻿using board_games.src.FeatureToggle;
+using System.Diagnostics;
+
+namespace board_games
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Board Games");
+            // Delete before merge
+            FeatureToggle features = new FeatureToggle();
+            features.AddFeature("feature1");
+            features.AddFeature("feature2");
+            Debug.Assert(features.GetLength() == 2);
+            try
+            {
+                features.AddFeature(null);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("Caught exception: {0}", e.Message);
+            }
+            try
+            {
+                features.AddFeature("");
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("Caught exception: {0}", e.Message);
+            }
+            Debug.Assert(features.GetLength() == 2);
+
+            Debug.Assert(features.GetFeatureState("feature1") == false);
+            Debug.Assert(features.GetFeatureState("feature2") == false);
+
+            features.EnableFeature("feature1");
+
+            Debug.Assert(features.GetFeatureState("feature1") == true);
+
+            features.DisableFeature("feature1");
+
+            Debug.Assert(features.GetFeatureState("feature1") == false);
         }
     }
 }
